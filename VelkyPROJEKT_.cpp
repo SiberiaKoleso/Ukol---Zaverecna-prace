9bba0bea8c681a94c35d711539f73b7abeb8c351
SiberiaKoleso <koki.cz.pro@gmail.com>
2026-04-15 20:47:44 +0100

    Create VelkyPROJEKT_.cpp

---
+++ VelkyPROJEKT_.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <ctime>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+
+using namespace std;
+// ---------- HRAC ----------
+
+// ---------- LEVEL SYSTEM ----------
+
+// ---------- VESNICE ----------
+
+// ---------- SCHOPNOST ----------
+
+// ---------- BOJ ----------
+
+// ---------- BOSS ----------
+
+// ---------- MAIN ----------
+int main() {
+
+
+}
